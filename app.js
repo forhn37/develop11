18cb0dd897bd02056f5ad1ae6c76953acde680a7
kimhyun <forhn37@gmail.com>
2023-10-10 22:11:18 +0900

    callback hell 작성

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("hello world");
+setTimeout(function() {
+  console.log("첫번째 함수");
+  setTimeout(function() {
+    console.log("두번째 함수");
+    setTimeout(function() {
+      console.log("세번째 함수");
+      setTimeout(function() {
+        console.log("네번째 함수");
+      },2000)
+    },1000)
+  }, 3000)
+}, 1000)
